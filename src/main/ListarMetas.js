c12a8b3ad2e6984f4cf865261f3c43d14ae6275c
Francisco Valdes <80586612+panchoValdes@users.noreply.github.com>
2022-11-24 22:29:26 -0300

    Update ListarMetas.js

--- src/main/ListarMetas.js
+++ src/main/ListarMetas.js
@@ -1,24 +1,48 @@
-import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+
+
+import { View, Text, TextInput, StyleSheet, Button, ScrollView, } from 'react-native';
+import React, { Component } from 'react';
 import { useNavigation } from '@react-navigation/native';
-import React from 'react';
+import { ListItem } from 'react-native-elements';
+
+import { collection, firestore, getFirestore, doc, getDocFromCache  } from 'firebase/firestore';
+import { database } from '../../firebase-config';
+import { firebaseConfig } from '../../firebase-config';
+
 
 function ListarMetas(){
     const navigation =useNavigation();
+    const [metas, setMetas] = React.useState([]);
+    const docRef = doc(database, "metas");
+    const ListMetas = async() =>{
+        try {
+            const collectionRef = collection(database, 'metas');
+            const doc = await getDocFromCache(docRef);
+          
+            // Document was found in the cache. If no cached document exists,
+            // an error will be returned to the 'catch' block below.
+            console.log("Cached document data:", doc.data());
+          } catch (e) {
+            console.log("Error getting cached document:", e);
+          }
+    }
+ 
 
     const crearMeta = () =>{
         navigation.navigate('CreandoMeta');
 
     }
 
-
   return (
     <View style={styles.container}>
         <View >
             <Text style={styles.welcome} > Crea tu meta </Text>
             <Text style={styles.instructions} > Aqui van el listado de metas </Text>
-            
+            <Text styles >{metas.map(metas => <Product key={metas.id} {...metas} />)}</Text>
+           
         </View>
         <View>
+        
             <Button onPress={crearMeta}  style={{fontSize:17, fontWeight: '400', color:'#FF7D61'}} title='Crear Meta' />
         </View>
     </View>
