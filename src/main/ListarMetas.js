cacd1251234d5f2eeade1845de9dcdb32739f3c7
Francisco Valdés <fvaldes@regcheq.com>
2023-04-22 16:58:51 -0300

    versiones  de expo

--- src/main/ListarMetas.js
+++ src/main/ListarMetas.js
@@ -1,81 +1,75 @@
-
-
-import { View, Text, TextInput, StyleSheet, Button, ScrollView, } from 'react-native';
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, Button, ScrollView } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import { ListItem } from 'react-native-elements';
-
-import { collection, firestore, getFirestore, doc, getDocFromCache  } from 'firebase/firestore';
+import { collection, query, onSnapshot } from 'firebase/firestore';
 import { database } from '../../firebase-config';
-import { firebaseConfig } from '../../firebase-config';
 
+function ListarMetas() {
+  const navigation = useNavigation();
+  const [metas, setMetas] = useState([]);
 
-function ListarMetas(){
-    const navigation =useNavigation();
-    const [metas, setMetas] = React.useState([]);
-    const docRef = doc(database, "metas");
-    const ListMetas = async() =>{
-        try {
-            const collectionRef = collection(database, 'metas');
-            const doc = await getDocFromCache(docRef);
-          
-            // Document was found in the cache. If no cached document exists,
-            // an error will be returned to the 'catch' block below.
-            console.log("Cached document data:", doc.data());
-          } catch (e) {
-            console.log("Error getting cached document:", e);
-          }
-    }
- 
+  useEffect(() => {
+    const q = query(collection(database, 'metas'));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const docs = [];
+      querySnapshot.forEach((doc) => {
+        docs.push({ ...doc.data(), id: doc.id });
+      });
+      setMetas(docs);
+    });
+    return unsubscribe;
+  }, []);
 
-    const crearMeta = () =>{
-        navigation.navigate('CreandoMeta');
-
-    }
+  const crearMeta = () => {
+    navigation.navigate('CreandoMeta');
+  };
 
   return (
     <View style={styles.container}>
-        <View >
-            <Text style={styles.welcome} > Crea tu meta </Text>
-            <Text style={styles.instructions} > Aqui van el listado de metas </Text>
-            <Text styles >{metas.map(metas => <Product key={metas.id} {...metas} />)}</Text>
-           
-        </View>
-        <View>
-        
-            <Button onPress={crearMeta}  style={{fontSize:17, fontWeight: '400', color:'#FF7D61'}} title='Crear Meta' />
-        </View>
+      <View>
+        <Text style={styles.welcome}>Lista de Metas</Text>
+        <ScrollView>
+          {metas.map((meta) => (
+            <View key={meta.id} style={styles.metaContainer}>
+              <Text style={styles.metaTitulo}>{meta.titulo}</Text>
+              <Text style={styles.metaDescripcion}>{meta.descripcion}</Text>
+            </View>
+          ))}
+        </ScrollView>
+      </View>
+      <View>
+        <Button onPress={crearMeta} title="Crear Meta" />
+      </View>
     </View>
-  )
+  );
 }
 
-export default ListarMetas
-
 const styles = StyleSheet.create({
-    instructions:{
-        textAlign: 'center',
-        color: '#333333',
-        margin:10,
-    },
-    welcome:{
-        fontSize: 20,
-        textAlign:'center',
-        margin:10,
-    },
-    button:{
-        width:250,
-        height:40,
-        borderRadius:10,
-        backgroundColor:'#FFA495',
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginVertical: 10,
-        borderColor:'#fff',
-        borderWidth: 1,
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF',
+    padding: 10,
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  metaContainer: {
+    borderWidth: 1,
+    borderColor: '#d3d3d3',
+    borderRadius: 4,
+    padding: 10,
+    marginVertical: 5,
+  },
+  metaTitulo: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  metaDescripcion: {
+    fontSize: 16,
+  },
+});
 
-    container: {
-      flex: 2,
-
-    },
-})
+export default ListarMetas;
