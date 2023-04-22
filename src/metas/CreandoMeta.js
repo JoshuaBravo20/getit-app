c5c995cf81384bbb01d60f26c2096ac82d8aa08c
Francisco Valdés <fvaldes@regcheq.com>
2023-04-22 18:41:50 -0300

    esperando conflictos

--- src/metas/CreandoMeta.js
+++ src/metas/CreandoMeta.js
@@ -10,46 +10,46 @@ const CreandoMeta = () => {
         firestore(nameMeta).collection('metas').add({
             nombreMeta: nameMeta,
         })
-      };
+    };
 
- 
-    
-  return (
-    <View>
-        <View style={styles.container1}>
-            <Text style={{fontSize:30, fontWeight: '400', textAlign:'center'}}>Nombre de la meta</Text>
-            <TextInput onChangeText={(text) => setNameMeta(text)} placeholder="Crea un nombre a tu meta " style={styles.input}></TextInput>
-            <Text style={{fontSize:17, fontWeight: '300', textAlign:'center', margin:10}}>¿Como desea subir su progreso?</Text>
-            <Button  style={{fontSize:17, fontWeight: '400', color:'#FF7D61'}} onPress={addMeta} title='Crear Meta' />
+
+
+    return (
+        <View>
+            <View style={styles.container1}>
+                <Text style={{ fontSize: 30, fontWeight: '400', textAlign: 'center' }}>Nombre de la meta</Text>
+                <TextInput onChangeText={(text) => setNameMeta(text)} placeholder="Crea un nombre a tu meta " style={styles.input}></TextInput>
+                <Text style={{ fontSize: 17, fontWeight: '300', textAlign: 'center', margin: 10 }}>¿Como desea subir su progreso?</Text>
+                <Button style={{ fontSize: 17, fontWeight: '400', color: '#FF7D61' }} onPress={addMeta} title='Crear Meta' />
+            </View>
+
         </View>
- 
-    </View>
-  )
+    )
 }
 
 export default CreandoMeta
 const styles = StyleSheet.create({
 
-    input:{
+    input: {
         backgroundColor: 'white',
-        width:350,
-        height:45,
+        width: 350,
+        height: 45,
         borderColor: '#e8e8e8',
-        borderWidth:1,
-        borderRadius:5,
-        paddingHorizontal:15,
+        borderWidth: 1,
+        borderRadius: 5,
+        paddingHorizontal: 15,
         marginVertical: 10,
     },
-    container1:{
-        alignItems:'center',
-        padding:30,
+    container1: {
+        alignItems: 'center',
+        padding: 30,
     },
     container2: {
         flex: 1,
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
-      },
+    },
 
 })
 //kjlsdakljdkslajdsa
