bbc6d919998cf629c1959283a987ff7dcf2073d4
Francisco Valdés <fvaldes@regcheq.com>
2023-04-22 21:11:13 -0300

    arreglo de boton en el listar metas

--- src/main/ListarMetas.js
+++ src/main/ListarMetas.js
@@ -1,8 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, Button, ScrollView } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import { collection, query, onSnapshot, getFirestore } from 'firebase/firestore';
-import { firebaseConfig } from '../../firebase-config';
+import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet, Button, ScrollView } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import {
+  collection,
+  query,
+  onSnapshot,
+  getFirestore,
+} from "firebase/firestore";
+import { firebaseConfig } from "../../firebase-config";
 import { initializeApp } from "firebase/app";
 
 function ListarMetas() {
@@ -12,7 +17,7 @@ function ListarMetas() {
   const [metas, setMetas] = useState([]);
 
   useEffect(() => {
-    const metasCollectionRef = collection(database, 'metas');
+    const metasCollectionRef = collection(database, "metas");
     const q = query(metasCollectionRef);
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
       const docs = [];
@@ -25,28 +30,31 @@ function ListarMetas() {
   }, []);
 
   const crearMeta = () => {
-    navigation.navigate('CreandoMeta');
+    navigation.navigate("CreandoMeta");
   };
 
   console.log("Metas => ", metas);
 
   return (
     <View style={styles.container}>
-      <View>
-      <View>
-        <Button onPress={crearMeta} title="Crear Meta" />
-      </View> 
-      <Text style={styles.welcome}>Lista de Metas</Text>
-        <ScrollView>
+      
+      <ScrollView>
+        <View>
+          
+          <Text style={styles.welcome}>Lista de Metas</Text>
           {metas.map((meta) => (
             <View key={meta.id} style={styles.metaContainer}>
-              <Text style={styles.metaTitulo}>{meta.nombreMeta ? meta.nombreMeta : "Titulo acá"}</Text>
-              <Text style={styles.metaDescripcion}>{meta.descripcion ? meta.descripcion : 'Descripción acá'}</Text>
+              <Text style={styles.metaTitulo}>
+                {meta.nombreMeta ? meta.nombreMeta : "Titulo acá"}
+              </Text>
+              <Text style={styles.metaDescripcion}>
+                {meta.descripcion ? meta.descripcion : "Descripción acá"}
+              </Text>
             </View>
           ))}
-        </ScrollView>
-      </View>
-      
+        </View>
+      </ScrollView>
+      <Button onPress={crearMeta} title="Crear Meta" />
     </View>
   );
 }
@@ -54,24 +62,24 @@ function ListarMetas() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F5FCFF',
+    backgroundColor: "#F5FCFF",
     padding: 10,
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
+    textAlign: "center",
     margin: 10,
   },
   metaContainer: {
     borderWidth: 1,
-    borderColor: '#d3d3d3',
+    borderColor: "#d3d3d3",
     borderRadius: 4,
     padding: 10,
     marginVertical: 5,
   },
   metaTitulo: {
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 5,
   },
   metaDescripcion: {
